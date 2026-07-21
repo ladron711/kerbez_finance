54dad9cca411c855a6ba310beee2fb55634d8437
ladron711 <ladronladron711@gmail.com>
2026-07-21 17:39:30 +0500

    added filters in adminpanel

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 from .models import User, Worker, Category, Transaction
 
 
-admin.site.register(User)
-admin.site.register(Worker)
-admin.site.register(Category)
-admin.site.register(Transaction)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "user_id")
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "type")
+    list_filter = ("type",)
+    search_fields = ("name",)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "user", "category", "worker", "amount")
+    list_filter = ("category__type", "category", "worker", "created_at")
+    search_fields = ("comment",)
+    date_hierarchy = "created_at"
